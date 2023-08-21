c3dce5bb363da976ff777ab684d1a626edf10bf2
Harshit-3905 <harshitjoshileet@gmail.com>
2023-08-21 22:33:39 +0530

    Added User Schema

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const { chats } = require("./data/data");
 
-const { chats } = require('./data/data');
-
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.PORT || 5000;
-app.get('/', (req, res) =>{
-    res.send('Hello World! ');
+const connectDB = require("./config/db");
+connectDB();
+
+const userRoutes = require("./routes/userRoutes");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World! ");
 });
 
-app.get("/api/chat", (req, res) =>{
-    res.send(chats);
-});
-app.get("/api/chat/:id", (req, res) =>{
-    // console.log(req.params.id);
-    const singleChat = chats.find((chat) => chat._id === req.params.id);
-    res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 
-app.listen(port, () => 
-    console.log(`Listening on port ${port}`)
-);
+app.listen(port, () => console.log(`Listening on port ${port}`));
